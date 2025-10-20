b2722cfa1fb9492ddd5eb0c1cff9bd6f36d297ff
MVL-R <rodulfamarkvincent@gmail.com>
2025-10-20 22:35:28 +0800

    Added Sales Logging Junk Food category

---
+++ app/src/main/java/com/example/sarisarisales/SalesLoggingActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sarisarisales
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SalesLoggingActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sales_logging_junk_food)
+    }
+}
