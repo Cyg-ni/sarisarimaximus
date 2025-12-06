6579143d37b19d9d6305878bfe2cae76679254a0
MVL-R <rodulfamarkvincent@gmail.com>
2025-12-06 19:20:24 +0800

    Added Firebase Gradle Import

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -41,7 +43,8 @@ android {
 }
 
 dependencies {
-    implementation("com.google.android.material:material:1.13.0") // Material 3
+    implementation(platform("com.google.firebase:firebase-bom:34.6.0"))
+    implementation("com.google.firebase:firebase-analytics")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
